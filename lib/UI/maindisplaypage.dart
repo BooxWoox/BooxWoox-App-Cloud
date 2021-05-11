7479e71ecea5fb33e8efd2f33986578c3db94e4d
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-05-11 06:35:27 +0530

    Card UI in main display page.

--- lib/UI/maindisplaypage.dart
+++ lib/UI/maindisplaypage.dart
@@ -11,17 +11,19 @@ import 'AddBookPage.dart';
 import '../Models/Book_info_model.dart';
 import 'package:unicorndial/unicorndial.dart';
 
-final _firestore=FirebaseFirestore.instance;
+final _firestore = FirebaseFirestore.instance;
 
 class maindisplaypage extends StatefulWidget {
-  static String id='maindisplaypage_Screen';
+  static String id = 'maindisplaypage_Screen';
+
   @override
   _maindisplaypageState createState() => _maindisplaypageState();
 }
 
 class _maindisplaypageState extends State<maindisplaypage> {
-  List<String> Homepage_Cat=[];
-  List<homepage_items_featured> featured=[];
+  List<String> Homepage_Cat = [];
+  List<homepage_items_featured> featured = [];
+
   @override
   void initState() {
     //initiaise to get list of homepage categories from database
@@ -31,6 +33,7 @@ class _maindisplaypageState extends State<maindisplaypage> {
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     var floatingButtons = List<UnicornButton>();
@@ -41,7 +44,7 @@ class _maindisplaypageState extends State<maindisplaypage> {
         currentButton: FloatingActionButton(
           onPressed: () {
             print('add books rent');
-            Navigator.pushNamed(context,AddBookPage.id);
+            Navigator.pushNamed(context, AddBookPage.id);
           },
           heroTag: "addrentbooks",
           backgroundColor: Colors.deepOrange,
@@ -57,7 +60,7 @@ class _maindisplaypageState extends State<maindisplaypage> {
         currentButton: FloatingActionButton(
           onPressed: () {
             print('addbooksell');
-            Navigator.pushNamed(context,AddBookPage.id);
+            Navigator.pushNamed(context, AddBookPage.id);
           },
           heroTag: "addbookssell",
           backgroundColor: Colors.orange,
@@ -66,179 +69,204 @@ class _maindisplaypageState extends State<maindisplaypage> {
         ),
       ),
     );
-    if(Homepage_Cat.isEmpty){
+    if (Homepage_Cat.isEmpty) {
       return Scaffold(
         body: Center(
-          child: Text("Fetching Data!",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),),
+          child: Text(
+            "Fetching Data!",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
       );
-    }else
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: UnicornDialer(
-          backgroundColor: Colors.transparent,
-          parentButtonBackground: Colors.amber,
-          orientation: UnicornOrientation.VERTICAL,
-          parentButton: Icon(Icons.playlist_add_sharp),
-          childButtons: floatingButtons),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 45,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal:20.0),
-                child: TextField(
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.search,color: Color(0xff7D7D7D),),
-                    fillColor: Color(0xffEBEBEB),
-                    hintText: "Search",
-                    filled: true,
-                    contentPadding: EdgeInsets.symmetric(vertical:4.0,horizontal: 25),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      borderSide: BorderSide(color:  Color(0xff707070)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      borderSide: BorderSide(color: Color(0xff707070)),
+    } else
+      return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: UnicornDialer(
+            backgroundColor: Colors.transparent,
+            parentButtonBackground: Colors.amber,
+            orientation: UnicornOrientation.VERTICAL,
+            parentButton: Icon(Icons.playlist_add_sharp),
+            childButtons: floatingButtons),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 45,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Color(0xff7D7D7D),
+                      ),
+                      fillColor: Color(0xffEBEBEB),
+                      hintText: "Search",
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 4.0, horizontal: 25),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Color(0xff707070)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Color(0xff707070)),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount:Homepage_Cat.length,
-                itemBuilder: (context,index){
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 18),
-                        child: Text(Homepage_Cat[index],
-                        style: TextStyle(
-                          fontFamily: "LeelawUI",
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold
-                        ),),
-                      ),
-                      Container(
-                        height:220,
-                        child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount:featured.length,
-                            itemBuilder: (context,itemIndex){
-                              return InkWell(
-                                onTap: (){
-                                  //goto book info page
-                                  Navigator.pushNamed(context,Book_info.id,arguments: maindisp_book_info_model(featured[itemIndex]));
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Card(
-                                    elevation: 3,
-                                    color:Colors.white,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(21),
-                                    ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: Homepage_Cat.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 18),
+                          child: Text(
+                            Homepage_Cat[index],
+                            style: TextStyle(
+                                fontFamily: "LeelawUI",
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          height: 220,
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: featured.length,
+                              itemBuilder: (context, itemIndex) {
+                                return InkWell(
+                                  onTap: () {
+                                    //goto book info page
+                                    Navigator.pushNamed(context, Book_info.id,
+                                        arguments: maindisp_book_info_model(
+                                            featured[itemIndex]));
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
                                     child: Column(
                                       mainAxisSize: MainAxisSize.min,
                                       children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
+                                        Card(
+                                          elevation: 3,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16),
+                                          ),
                                           child: Container(
                                             height: 140,
-                                            width:150,
-                                            child: Image.network(featured[itemIndex].ImageURl,
-                                            fit: BoxFit.contain,),
+                                            width: 100,
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(16),
+                                              child: Image.network(
+                                                featured[itemIndex].ImageURl,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
                                           ),
                                         ),
-                                        Padding(
-                                          padding: const EdgeInsets.all(3.0),
-                                          child: SizedBox(
-                                              width:150,
-                                              child: Center(child: Text(featured[itemIndex].BookName,
+                                        SizedBox(
+                                          width: 100,
+                                          child: Center(
+                                            child: Text(
+                                              featured[itemIndex].BookName,
                                               style: TextStyle(
                                                 color: Colors.black87,
                                                 fontSize: 16,
                                                 fontFamily: "LeelawUI",
-
-                                              ),))),
-                                        )
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 100,
+                                          child: Center(
+                                            child: Text(
+                                              featured[itemIndex].Author,
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 14,
+                                                fontFamily: "LeelawUI",
+                                              ),
+                                            ),
+                                          ),
+                                        ),
                                       ],
-                                    )
+                                    ),
                                   ),
-                                ),
-                              );
-                            }),
-                      )
-                    ],
-                  );
-                }
+                                );
+                              }),
+                        )
+                      ],
+                    );
+                  }),
             ),
-          ),
-        ],
-
-      ),
-    );
-
+          ],
+        ),
+      );
   }
 
-
-
-  void Featured_items_get() async{
-    try{
-      await _firestore.collection("Book_Collection").get().then((value){
-        for(var i in value.docs){
+  void Featured_items_get() async {
+    try {
+      await _firestore.collection("Book_Collection").get().then((value) {
+        for (var i in value.docs) {
           print(i.get("BookName"));
-          String bkname=i.get("BookName");
-          String author=i.get("Author");
-          String ImageUrl=i.get("ImageUrl");
-          String coll_type=i.get("Homepage_category");
-          String original_loc=i.id.toString().trim();
-          String owneruid=i.get("OwnerUID");
-          String selleraddress=i.get("seller_address");
-          String sellerphn=i.get("seller_phoneNumber");
-          bool availability=i.get("Availability");
-          featured.add(homepage_items_featured(author, bkname, coll_type, ImageUrl,original_loc,owneruid,selleraddress,sellerphn,availability));
-
+          String bkname = i.get("BookName");
+          String author = i.get("Author");
+          String ImageUrl = i.get("ImageUrl");
+          String coll_type = i.get("Homepage_category");
+          String original_loc = i.id.toString().trim();
+          String owneruid = i.get("OwnerUID");
+          String selleraddress = i.get("seller_address");
+          String sellerphn = i.get("seller_phoneNumber");
+          bool availability = i.get("Availability");
+          featured.add(homepage_items_featured(
+              author,
+              bkname,
+              coll_type,
+              ImageUrl,
+              original_loc,
+              owneruid,
+              selleraddress,
+              sellerphn,
+              availability));
         }
         setState(() {
           print(featured);
         });
-
       });
-    }catch(e){
+    } catch (e) {
       print(e.message);
     }
   }
-  void home_cat_get() async{
-    try{
-      await _firestore.collection("Homepage_item_list").get().then((value){
-        for(var i in value.docs){
+
+  void home_cat_get() async {
+    try {
+      await _firestore.collection("Homepage_item_list").get().then((value) {
+        for (var i in value.docs) {
           Homepage_Cat.add(i.get("Name"));
         }
-        setState(() {
-
-        });
+        setState(() {});
       });
-    }catch(e){
-      print("yohooooooooooooooooo"+e.message);
+    } catch (e) {
+      print("yohooooooooooooooooo" + e.message);
     }
 
     print(Homepage_Cat);
