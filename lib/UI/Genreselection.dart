dfc613811d667e98d831a2c63ed186415380c4c6
Shivam Singhal <shivam.singhal18012002@gmail.com>
2021-10-21 21:58:00 +0530

    Genre page images added

--- lib/UI/Genreselection.dart
+++ lib/UI/Genreselection.dart
@@ -13,8 +13,43 @@ class GenreSelectionpage extends StatefulWidget {
 
 class _GenreSelectionpageState extends State<GenreSelectionpage> {
   // final List<String> _genrename = ['Genre1','Genre2','Genre3','Genre4','Genre5','Genre5','Genre6','Genre7','Genre8','Genre9','Genre10','Genre11'];
+  final List<String> _genreimages = [
+    'assets/images/action.png',
+    'assets/images/fantasy.png',
+    'assets/images/science fiction.png',
+    'assets/images/dystopian.png',
+    'assets/images/action and adventure.png',
+    'assets/images/mystery.png',
+    'assets/images/horror.png',
+    'assets/images/thriller and suspense.png',
+    'assets/images/historical fiction.png',
+    'assets/images/romance.png',
+    'assets/images/women\'s fiction.png',
+    'assets/images/LGBTQ+.png',
+    'assets/images/contemporary fiction.png',
+    'assets/images/literary fiction.png',
+    'assets/images/magical realism.png',
+    'assets/images/graphic novel.png',
+    'assets/images/short story.png',
+    'assets/images/young adult.png',
+    'assets/images/new adult.png',
+    'assets/images/children\'s.png',
+    'assets/images/autobiography.png',
+    'assets/images/biography.png',
+    'assets/images/art ad photography.png',
+    'assets/images/travel.png',
+    'assets/images/history.png',
+    'assets/images/true crime.png',
+    'assets/images/humor.png',
+    'assets/images/essays.png',
+    'assets/images/religion.png',
+    'assets/images/humanities and social sciences.png',
+    'assets/images/parenting and families.png',
+  ];
   ColorFilter colr =
       ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken);
+  String name;
+  // String nameofimage = genres.data.value[index];
   List<int> _selectedgenre = [];
   @override
   Widget build(BuildContext context) {
@@ -30,42 +65,44 @@ class _GenreSelectionpageState extends State<GenreSelectionpage> {
         }
         return Column(
           children: [
-            Container(
-              // height: 300.0,
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 100.0,
-                        width: 100.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                "https://images.unsplash.com/photo-1579202673506-ca3ce28943ef"),
-                            fit: BoxFit.cover,
-                            colorFilter: colr,
+            Center(
+              child: Container(
+                // height: 300.0,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height*0.12,
+                          width: MediaQuery.of(context).size.width*0.20,
+                          // height: 100.0,
+                          // width: 100.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/genres.png'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                            // color: Colors.red,
                           ),
-                          borderRadius: BorderRadius.circular(10.0),
-                          // color: Colors.red,
                         ),
                       ),
-                    ),
-                    Flexible(
-                      child: Text(
-                        'What are your favourite genres?',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30.0,
+                      Flexible(
+                        child: Text(
+                          'What are your favourite genres?',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 27.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -74,7 +111,7 @@ class _GenreSelectionpageState extends State<GenreSelectionpage> {
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 20.0,
+                fontSize: 18.0,
               ),
             ),
             Expanded(
@@ -98,33 +135,69 @@ class _GenreSelectionpageState extends State<GenreSelectionpage> {
                           } else {
                             _selectedgenre.add(index);
                           }
+                          name = genres.data.value[index];
+                          // print(name);
+                          // print(index);
                         });
                       },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('UIAssets/genre/adventure.webp'),
-                            fit: BoxFit.cover,
-                            colorFilter: _selectedgenre.contains(index)
-                                ? null
-                                : ColorFilter.mode(
-                                    Colors.black.withOpacity(0.5),
-                                    BlendMode.darken),
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                          // color: Colors.red,
-                        ),
-                        margin: EdgeInsets.all(15.0),
-                        // height: 100.0,
-                        // width: 100.0,
-                        child: Center(
-                          child: Text(
-                            genres.data.value[index],
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 17.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex:7,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    // image: AssetImage('UIAssets/genre/adventure.webp'),
+                                    image: AssetImage(_genreimages[index]),
+                                    fit: BoxFit.contain,
+                                    // colorFilter: _selectedgenre.contains(index)
+                                    //     ? null
+                                    //     : ColorFilter.mode(
+                                    //         Colors.black.withOpacity(0.2),
+                                    //         BlendMode.darken),
+
+                                    colorFilter: _selectedgenre.contains(index)
+                                        ? ColorFilter.mode(
+                                        Colors.black.withOpacity(0.2),
+                                        BlendMode.darken)
+                                        : null,
+
+                                  ),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  // color: Colors.red,
+                                ),
+                                margin:
+                                    EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                                // child: FittedBox(
+                                //   child: Image.asset(_genreimages[index]),
+                                //   fit: BoxFit.fill,
+                                // ),
+                                // height: 100.0,
+                                // width: 100.0,
+                                // child: Center(
+                                //   child: Text(
+                                //     genres.data.value[index],
+                                //     style: TextStyle(
+                                //       color: Colors.white,
+                                //       fontSize: 17.0,
+                                //     ),
+                                //   ),
+                                // ),
+                              ),
                             ),
-                          ),
+                            Flexible(
+                              child: Text(
+                                genres.data.value[index],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  // color: Colors.white,
+                                  fontSize: 15.0,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     );
@@ -136,7 +209,28 @@ class _GenreSelectionpageState extends State<GenreSelectionpage> {
               padding: const EdgeInsets.all(8.0),
               child: GestureDetector(
                 onTap: () {
-                  Navigator.of(context).pushReplacementNamed(Homepage.id);
+
+                  //Here we will give error message if the number of genres selected is less than 3.
+                  if (_selectedgenre.length < 3) {
+                    return showDialog(
+                        context: context,
+                        builder: (context) {
+                          Future.delayed(Duration(seconds: 2), () {
+                            Navigator.of(context).pop(true);
+                          });
+                          return AlertDialog(
+                            title: Text(
+                                'Please choose atleast 3 genres.',
+                              textAlign: TextAlign.center,
+                            ),
+                          );
+                        });
+                  }
+
+                  //If number of genres selected is more than 3, no error.
+                  else {
+                    Navigator.of(context).pushReplacementNamed(Homepage.id);
+                  }
                 },
                 child: Container(
                   height: 50.0,
